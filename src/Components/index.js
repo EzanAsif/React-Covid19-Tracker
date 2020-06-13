beaaa77ad541498b8d10e96b530c11b8ea415978
EzanAsif <ezanasif1972@gmail.com>
2020-06-13 15:00:10 +0500

    First Commit

---
+++ src/Components/index.js
@@ -0,0 +1,7 @@
+// this file is going to take all components Cards,
+// Charts and COuntry PIcker And will be exporting from
+// components folder
+
+export { default as Cards} from './Cards/Cards';
+export { default as Chart} from './Chart/Chart';
+export { default as CountryPicker} from './CountryPicker/CountryPicker';
